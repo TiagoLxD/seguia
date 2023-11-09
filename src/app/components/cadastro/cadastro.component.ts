429ae0304be70215d89ce38b932dd0fc843493ff
tiagohlazarinidev <tiagolazarinidev@gmail.com>
2023-11-08 23:48:56 -0300

    adicionado efeito maquina de escrever

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -16,6 +16,8 @@ export class CadastroComponent implements OnInit, OnDestroy {
   camposTocados = { email: false, emailConfirm: false };
   cadastroForm!: FormGroup;
 
+  typedText: string = '';
+  originalText: string = 'Dê inicio ao seu futuro hoje mesmo! Estude do seu jeito e onde quiser, a hora que quiser.';
 
   constructor(private formBuilder: FormBuilder) {
     this.cadastroForm = this.formBuilder.group({
@@ -25,6 +27,7 @@ export class CadastroComponent implements OnInit, OnDestroy {
     });
   }
   ngOnInit(): void {
+    this.typeText();
   }
   ngOnDestroy(): void {
     if (this.sub$) {
@@ -45,6 +48,20 @@ export class CadastroComponent implements OnInit, OnDestroy {
       // })
       // this.sub$.add(sub);
     }
+  }
+
+  typeText() {
+    const speed = 50;
+    let i = 0;
+
+    const typeInterval = setInterval(() => {
+      if (i < this.originalText.length) {
+        this.typedText += this.originalText.charAt(i);
+        i++;
+      } else {
+        clearInterval(typeInterval);
+      }
+    }, speed);
   }
 
 }
