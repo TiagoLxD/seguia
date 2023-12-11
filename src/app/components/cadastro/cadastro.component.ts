641f0d4139ac1d705f5b7bf6be3d8c59d40d72a5
tiagohlazarinidev <tiagolazarinidev@gmail.com>
2023-12-11 07:53:07 -0300

    add redirect para depois do cadastro ir login

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { CadastroService } from './../core/services/cadastro.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -21,7 +22,8 @@ export class CadastroComponent implements OnInit, OnDestroy {
   originalText: string = 'Dê inicio ao seu futuro hoje mesmo! Estude do seu jeito e onde quiser, a hora que quiser.';
 
   constructor(private formBuilder: FormBuilder,
-    private _cadastroService: CadastroService) {
+    private _cadastroService: CadastroService,
+    private router: Router) {
     this.cadastroForm = this.formBuilder.group({
       completeName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -51,6 +53,7 @@ export class CadastroComponent implements OnInit, OnDestroy {
         .cadastrar(payload).subscribe(res => {
           if (res.message) {
             this.cadastroSucesso = true
+            this.router.navigate(['/login'])
           }
         })
       this.sub$.add(sub);
